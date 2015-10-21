b64018198173415e315a13becab9240a1ab39b88
Jérémy Marodon <marodon.jeremy@gmail.com>
2015-10-21 16:55:53 +0200

    [ADD] PHP-CS-Fixer tasks

--- Controller/ContactController.php
+++ Controller/ContactController.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * (c) Jérémy Marodon <marodon.jeremy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Th3Mouk\ContactBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
 use Th3Mouk\ContactBundle\Entity\ContactInterface;
 
 /**
